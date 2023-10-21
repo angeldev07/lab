1f10dddde5d63b8020e45d65fac807e063e4ffd5
angeldev07 <angelgaran20@gmail.com>
2023-10-20 23:07:02 -0500

    first commit

---
+++ LaboratorioSQL/src/main/java/com/laboratorio/LaboratorioSqlApplication.java
@@ -0,0 +1,13 @@
+package com.laboratorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaboratorioSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaboratorioSqlApplication.class, args);
+	}
+
+}
